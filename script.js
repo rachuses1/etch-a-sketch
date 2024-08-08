614b6bdbce461f195c6e00ae6e386c10c6fbf72d
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-08 20:13:29 +1000

    added function of adding color to grid when mouse hovers individual squares.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.querySelector('.container');
 
 function addDiv() {    
     const newDiv = document.createElement("div");
+    newDiv.classList.add("grid");
     container.appendChild(newDiv);
 };
 
@@ -10,3 +11,14 @@ for (let i = 0; i < 16*16; i++) {
     addDiv();
 };
 
+let grid = document.querySelectorAll("div.grid");
+let gridSquare = grid.children;
+
+document.addEventListener("mouseover", color);
+function color () {
+for(let i = 0; i < grid.length; i++) {
+    grid[i].addEventListener("mouseover", function () {
+        grid[i].style.backgroundColor = "aquamarine";
+    });
+};
+}
