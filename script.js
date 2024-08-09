6086003d0e2c971581b741c9fbae9c2c61f9d0a9
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-09 12:24:27 +1000

    add button to create new grid of user prompted size

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
+let new_size;
 
+let initial_size = 16;
+addDiv(initial_size);
 
-function addDiv() {    
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("grid");
-    container.appendChild(newDiv);
-};
+container.style.width = initial_size*22 + "px";
 
-for (let i = 0; i < 16*16; i++) {
-    addDiv();
-};
+function addDiv(size) {   
+    for (let i = 0; i < size ** 2; i++) { 
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("grid");
+        container.appendChild(newDiv);
+    };
+}; // create the grid
 
-let grid = document.querySelectorAll("div.grid");
-let gridSquare = grid.children;
+const grid = document.querySelectorAll('div.grid');
 
-document.addEventListener("mouseover", color);
+grid.forEach((square) => {
+    square.addEventListener("mouseover", color);
+});
+
 function color () {
-for(let i = 0; i < grid.length; i++) {
-    grid[i].addEventListener("mouseover", function () {
-        grid[i].style.backgroundColor = "aquamarine";
-    });
-};
+    for(let i = 0; i < grid.length; i++) {
+        grid[i].addEventListener("mouseover", function () {
+            grid[i].style.backgroundColor = "aquamarine";
+        });
+    };
 }
+
+button.addEventListener("click", function findSize() {
+    new_size = prompt("How many squares per side?",);
+    rid();
+    addDiv(new_size);
+    resize(new_size);
+});
+
+function rid () {
+    for(let i = 0; i < grid.length; i++) {
+        grid[i].remove();
+    };
+}; // remove previous grid 
+
+function resize (new_size) {
+    container.style.width = new_size*22 + "px";
+}; // resize container
+
+
+
